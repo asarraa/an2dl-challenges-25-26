8284ac7858faf79a1c7a373218434f64c0f1e452
AndreRossii <andrea.reds08@gmail.com>
2025-12-09 16:50:48 +0100

    Fixed testingpre for testset generation
    Co-authored-by: Fabio Rossi <fabio11.rossi@mail.polimi.it>

--- challenge2/testingpre.py
+++ challenge2/testingpre.py
@@ -84,52 +84,52 @@ def contains_slime(img_bgr, threshold=50):
     # Determine if the count exceeds the threshold
     return green_pixel_count > threshold
                        
-def process_slime_removal(img_bgr, mask_gray):
-    """
-    Detects and removes green marker ink ("slime") from the histology image.
+# def process_slime_removal(img_bgr, mask_gray):
+#     """
+#     Detects and removes green marker ink ("slime") from the histology image.
     
-    Why: Green ink acts as an artifact that can confuse the CNN.
-    How: Detects green pixels in HSV space and applies Inpainting.
+#     Why: Green ink acts as an artifact that can confuse the CNN.
+#     How: Detects green pixels in HSV space and applies Inpainting.
     
-    Args:
-        img_bgr (np.array): Original image.
-        mask_gray (np.array): Original tumor mask.
+#     Args:
+#         img_bgr (np.array): Original image.
+#         mask_gray (np.array): Original tumor mask.
         
-    Returns:
-        tuple: (img_clean, mask_clean) - Processed image and updated mask.
-    """
-    # Convert BGR to HSV color space to easily isolate the green color.
-    hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
+#     Returns:
+#         tuple: (img_clean, mask_clean) - Processed image and updated mask.
+#     """
+#     # Convert BGR to HSV color space to easily isolate the green color.
+#     hsv = cv2.cvtColor(img_bgr, cv2.COLOR_BGR2HSV)
     
-    # Define the lower and upper bounds for the green color (Marker Ink).
-    lower_green = np.array([35, 50, 50])
-    upper_green = np.array([90, 255, 255])
+#     # Define the lower and upper bounds for the green color (Marker Ink).
+#     lower_green = np.array([35, 50, 50])
+#     upper_green = np.array([90, 255, 255])
     
-    # Create a binary mask where green pixels are white (255).
-    mask_slime = cv2.inRange(hsv, lower_green, upper_green)
+#     # Create a binary mask where green pixels are white (255).
+#     mask_slime = cv2.inRange(hsv, lower_green, upper_green)
 
-    # Refine the slime mask:
-    # 1. Find contours to identify blobs.
-    # 2. Fill holes inside the blobs to make them solid.
-    contours, _ = cv2.findContours(mask_slime, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    mask_solid = np.zeros_like(mask_slime)
-    cv2.drawContours(mask_solid, contours, -1, (255), thickness=cv2.FILLED)
+#     # Refine the slime mask:
+#     # 1. Find contours to identify blobs.
+#     # 2. Fill holes inside the blobs to make them solid.
+#     contours, _ = cv2.findContours(mask_slime, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+#     mask_solid = np.zeros_like(mask_slime)
+#     cv2.drawContours(mask_solid, contours, -1, (255), thickness=cv2.FILLED)
     
-    # 3. Dilate the mask slightly to ensure we cover the edges of the ink.
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
-    mask_solid_final = cv2.dilate(mask_solid, kernel, iterations=1)
+#     # 3. Dilate the mask slightly to ensure we cover the edges of the ink.
+#     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5, 5))
+#     mask_solid_final = cv2.dilate(mask_solid, kernel, iterations=1)
 
-    # Apply Telea Inpainting: Replaces the masked "slime" pixels with 
-    # plausible texture derived from the surrounding neighborhood.
-    img_clean = cv2.inpaint(img_bgr, mask_solid_final, 3, cv2.INPAINT_TELEA)
+#     # Apply Telea Inpainting: Replaces the masked "slime" pixels with 
+#     # plausible texture derived from the surrounding neighborhood.
+#     img_clean = cv2.inpaint(img_bgr, mask_solid_final, 3, cv2.INPAINT_TELEA)
     
-    # Update the tumor mask:
-    # If a tumor region was covered by slime, we remove it (set to 0) 
-    # because the inpainting might have generated fake tissue.
-    mask_clean = mask_gray.copy()
-    mask_clean[mask_solid_final == 255] = 0
+#     # Update the tumor mask:
+#     # If a tumor region was covered by slime, we remove it (set to 0) 
+#     # because the inpainting might have generated fake tissue.
+#     mask_clean = mask_gray.copy()
+#     mask_clean[mask_solid_final == 255] = 0
     
-    return img_clean, mask_clean
+#     return img_clean, mask_clean
 
 def is_patch_valid_hsv(img_crop):
     """
@@ -233,7 +233,7 @@ def analyze_image_memory(img_bgr):
 # 4. TILING ENGINE
 # =============================================================================
 
-def process_single_slide(img_path, mask_path, label, output_img_dir, output_mask_dir, discard_dir, arrays_dir, is_test_set=False):
+def process_single_slide(img_path, mask_path, label, output_img_dir, output_mask_dir, arrays_dir, is_test_set=False):
     """
     Orchestrates the processing pipeline for a single whole-slide image (WSI) or ROI.
     Pipeline: Load -> Remove Slime -> Check Quality (Shrek) -> Tile -> Save.
@@ -251,29 +251,32 @@ def process_single_slide(img_path, mask_path, label, output_img_dir, output_mask
         list: A list of dictionaries containing metadata for the generated tiles.
               Returns "SHREK" string if the image was discarded.
     """
-    img_bgr = load_image_cv2(img_path)
-    mask_gray = load_mask_cv2(mask_path)
+    img = load_image_cv2(img_path)
+    mask = load_mask_cv2(mask_path)
     
     # Basic error check
-    if img_bgr is None or mask_gray is None: 
+    if img is None or mask is None: 
         return None
 
-    # --- Step 1: Slime Removal ---
-    img_clean, mask_clean = process_slime_removal(img_bgr, mask_gray)
+    if not is_test_set:
+        # --- Step 1: Slime Removal ---
+        if contains_slime(img):
+            return
+        #img_clean, mask_clean = process_slime_removal(img_bgr, mask_gray)
 
-    # --- Step 2: Quality Control (Shrek Check) ---
-    cls, _, _, _ = analyze_image_memory(img_clean)
-    
-    # If classified as "SHREK" (corrupted/artifact), we discard the whole slide.
-    if cls == "SHREK":
-        # Save the full clean image to discard folder for manual inspection
-        cv2.imwrite(str(discard_dir / img_path.name), img_clean)
-        # Even for test set, we flag it. 
-        return "SHREK"
+        # --- Step 2: Quality Control (Shrek Check) ---
+        cls, _, _, _ = analyze_image_memory(img)
+        
+        # If classified as "SHREK" (corrupted/artifact), we discard the whole slide.
+        if cls == "SHREK":
+            # Save the full clean image to discard folder for manual inspection
+            #cv2.imwrite(str(discard_dir / img_path.name), img)
+            # Even for test set, we flag it. 
+            return "SHREK"
 
     # --- Step 3: Tiling (Patch Extraction) ---
     tiles_data = []
-    h, w, _ = img_clean.shape
+    h, w, _ = img.shape
     base_name = img_path.stem # e.g., "img_001"
     img_array = []
 
@@ -287,8 +290,8 @@ def process_single_slide(img_path, mask_path, label, output_img_dir, output_mask
             if (y_end - y) < TILE_SIZE // 2 or (x_end - x) < TILE_SIZE // 2: continue
 
             # Extract the crop
-            img_crop = img_clean[y:y_end, x:x_end]
-            mask_crop = mask_clean[y:y_end, x:x_end]
+            img_crop = img[y:y_end, x:x_end]
+            mask_crop = mask[y:y_end, x:x_end]
 
             # --- Padding Logic ---
             # If the crop is smaller than TILE_SIZE (at the edges), we need to pad.
@@ -383,7 +386,7 @@ def main(do_test=True, preprocess_name=None):
     test_arrays_dir = single_preprocessing_dir / "test/arrays"
 
     # Discarded
-    discard_dir = single_preprocessing_dir / "discarded"
+    #discard_dir = single_preprocessing_dir / "discarded"
     '''
     data --> BASE_DATA
     -dataset --> base_dataset
@@ -410,7 +413,7 @@ def main(do_test=True, preprocess_name=None):
 
 
     # Clean up previous runs and create directories
-    for d in [out_train_img, out_train_mask, discard_dir, train_arrays_dir]:
+    for d in [out_train_img, out_train_mask, train_arrays_dir]:
         if d.exists(): 
             shutil.rmtree(d)
             print(f"[DEBUG] testing_pre: Cleaning output directory {d}!!!!!")
@@ -465,7 +468,7 @@ def main(do_test=True, preprocess_name=None):
             # Process the slide
             res = process_single_slide(
                 img_path, mask_path, label, 
-                out_train_img, out_train_mask, discard_dir, train_arrays_dir,
+                out_train_img, out_train_mask, train_arrays_dir,
                 is_test_set=False,
             )
             
