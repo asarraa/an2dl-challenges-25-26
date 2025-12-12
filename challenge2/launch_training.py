1d5125598e0c7d0788402a46d4921128d687479e
AndreRossii <andrea.reds08@gmail.com>
2025-12-12 21:12:55 +0100

    Adding dropout as parameter for resnet, added hyperparameters to log on comet

--- challenge2/launch_training.py
+++ challenge2/launch_training.py
@@ -192,6 +192,7 @@ def start_training(model_name="CNN", model_params=None, training_params=None, de
     }
 
     comet_experiment.log_parameters(hyper_params)
+    comet_experiment.log_parameters(current_model_cfg)
     #data_input_shape =  data_input_shape
 
     # -------------------------------------------------------
